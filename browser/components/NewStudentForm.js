df906ccc476dbbdb69ae60bb69209654d95a9309
Hariet <harietlewis@gmail.com>
2019-03-23 14:27:24 -0400

    Cycle 2

--- browser/components/NewStudentForm.js
+++ browser/components/NewStudentForm.js
@@ -1,22 +1,59 @@
 import React, { Component } from 'react';
 
+
 export default class NewStudentForm extends Component {
+  constructor() {
+      super()
+      this.state = {
+          firstName: '',
+          lastName: '',
+          email: ''
+      }
+      this.handleSubmit = this.handleSubmit.bind(this)
+      this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(event)  {
+      this.setState ({ 
+          [event.target.name]: event.target.value
+      })
+  }
+  handleSubmit(event) {
+      event.preventDefault() 
+      // this.props.newStudent(this.state)
+      this.setState({
+          firstName: '',
+          lastName: '',
+          email: ''
+      })
+  }
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
           First Name:
-          <input type="text" name="firstName" />
+          <input
+          type="text" 
+          name="firstName"
+          onChange={this.handleChange}
+          value={this.state.firstName} />
         </label>
 
         <label>
           Last Name:
-          <input type="text" name="lastName" />
+          <input 
+          type="text" 
+          name="lastName"
+          onChange={this.handleChange}
+          value={this.state.lastName} />
         </label>
 
         <label>
           Email:
-          <input type="email" name="email" />
+          <input 
+          type="email" 
+          name="email" 
+          onChange={this.handleChange}
+          value={this.state.email} />
         </label>
 
         <button type="submit">Submit New Student</button>
@@ -24,3 +61,4 @@ export default class NewStudentForm extends Component {
     );
   }
 }
+
